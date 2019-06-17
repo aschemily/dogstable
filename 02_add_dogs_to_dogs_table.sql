fb0d6172ef98d273bc7b0c806cf0b7cd6b1c28dc
aschemily <aschemily@gmail.com>
2019-06-17 19:18:56 -0400

    first commit

---
+++ 02_add_dogs_to_dogs_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO dogs (name, age, breed, color) VALUES ('Penny', 3, 'hound', 'brown');
+
+INSERT INTO dogs (name, age, breed, color) VALUES ('Bently', 3, 'corgi', 'black');
+
+INSERT INTO dogs (name, age, breed, color) VALUES ('Chico', 5, 'chihuaha', 'tan');
